fbb13be3ac2a9823650adbb4f1f89564f7dadd07
abisheksudarshan <abishek_1274@yahoo.com>
2020-07-04 19:13:37 +0530

    Create African Cities.sql

---
+++ SQL/Basic Join/African Cities.sql
@@ -0,0 +1,8 @@
+/*Given the CITY and COUNTRY tables, query the names of all cities where the CONTINENT is 'Africa'.
+
+Note: CITY.CountryCode and COUNTRY.Code are matching key columns.*/
+
+Solution:
+
+select name from city 
+where countrycode in (select code from country where continent='Africa');
