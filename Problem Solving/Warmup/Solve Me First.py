dc4e2e6286a826c5cbf2f99ab96e6adb662ab7d3
abisheksudarshan <abishek_1274@yahoo.com>
2020-07-04 19:30:56 +0530

    Create Solve Me First.py

---
+++ Problem Solving/Warmup/Solve Me First.py
@@ -0,0 +1,26 @@
+/*Complete the function solveMeFirst to compute the sum of two integers.
+
+Function prototype:
+
+int solveMeFirst(int a, int b);
+
+where,
+
+    a is the first integer input.
+    b is the second integer input
+
+Return values
+
+    sum of the above two integers
+*/
+
+Solution:
+
+def solveMeFirst(a,b):
+	# Hint: Type return a+b below
+    return (num1+num2)
+
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1,num2)
+print(res)
