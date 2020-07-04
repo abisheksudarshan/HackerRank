5f89a709f1f2cc9d1a903612c383f0d5cb9a4978
abisheksudarshan <abishek_1274@yahoo.com>
2020-07-04 19:23:49 +0530

    Create Draw The Triangle 2.sql

---
+++ SQL/Alternate Queries/Draw The Triangle 2.sql
@@ -0,0 +1,18 @@
+/*P(R) represents a pattern drawn by Julia in R rows. The following pattern represents P(5):
+
+* 
+* * 
+* * * 
+* * * * 
+* * * * *
+
+Write a query to print the pattern P(20).*/
+
+Solution:
+
+DECLARE @i INT = 1
+WHILE (@i <= 20) 
+BEGIN
+   PRINT REPLICATE('* ', @i) 
+   SET @i = @i + 1
+END
