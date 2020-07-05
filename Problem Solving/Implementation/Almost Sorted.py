a33cc3448cab99dc33094c346e92a509da20004e
abisheksudarshan <abishek_1274@yahoo.com>
2020-07-05 09:46:46 +0530

    Update Almost Sorted.py

--- Problem Solving/Implementation/Almost Sorted.py
+++ Problem Solving/Implementation/Almost Sorted.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
-
 #!/bin/python3
 
 import math
@@ -13,71 +6,45 @@ import random
 import re
 import sys
 
-#checking if the table is in ascending
-def checkorder(arr):
-    for i in range(0,len(arr)-1):
-        if arr[i]>arr[i+1]:
-            return False
-    else:
-        return True
-
-#checking if 1 swap is enough
-def checkswap(arr,i):
-    arr_copy=arr.copy()
-    temp=arr_copy[i]
-    arr_copy[i]=arr_copy[i+1]
-    arr_copy[i+1]=temp
-    return checkorder(arr_copy)
-
-#checking if reverse is an option    
-def checkreverse(arr,i):
-    arr_copy=arr.copy()
-    temp_arr=[]
-    
-    #identify susbet
-    for j in range(i,len(arr_copy)-1):
-        if arr_copy[j+1]<arr_copy[j]:
-            temp_arr.append(arr[j])
-    temp_arr.append(arr_copy[j])
-    
-    temp_arr.sort()    
-    
-    new_arr=arr_copy[:i]
-    
-    for t in temp_arr:
-        new_arr.append(t)
-    
-    for k in range(j+1,len(arr_copy)):
-        new_arr.append(arr_copy[k])
-    #print(new_arr)
-    return checkorder(new_arr),j
-
 # Complete the almostSorted function below.
-def almostSorted(arr):
-    swap=False
-    for i in range(0,len(arr)):
-        if arr[i]>arr[i+1]:
-            swap=checkswap(arr,i)
-            
+def almostSorted(a):
+    x = 0
+    y = len(a)-1
+    while a[x]<=a[x+1]:
+        x+=1
+    while a[y]>=a[y-1]:
+        y-=1
+
+    b = a[x:y+1]
+    b[0],b[-1]=b[-1],b[0]
+
+    cond_l = True if x-1<0 else b[0]>=a[x-1]
+    cond_r = True if y+1==len(a) else a[y+1]>=b[-1]
+    if cond_l and cond_r:
+        for i in range(len(b)-1):
+            if b[i]>b[i+1]:
+                break
+        else:
+            return "yes\nswap %s %s"%(x+1,y+1)
+        
+    b[0],b[-1]=b[-1],b[0]
     
-    if swap==True:
-        print('yes')
-        print('swap {} {}'.format(i+1,i+2))
+    cond_l = True if x-1<0 else a[x-1]<=b[-1]
+    cond_r = True if y+1==len(a) else  b[0]<=a[y+1]
+    if cond_l and cond_r:
+        for i in range(len(b)-1):
+            if b[i]<b[i+1]:
+                break
+        else:
+            return "yes\nreverse %s %s"%(x+1,y+1)
+    return "no"
+
     
-    if swap==False:
-       #print(arr)
-       reverse,j=checkreverse(arr,i) 
-       #print(reverse,j)
-       if reverse==True:
-        print('yes')
-        print('reverse {} {}'.format(i+1,j+1))
-    
-       else:
-        print('no')
+     
 
 if __name__ == '__main__':
     n = int(input())
 
     arr = list(map(int, input().rstrip().split()))
 
-    almostSorted(arr)
+    print(almostSorted(arr))
