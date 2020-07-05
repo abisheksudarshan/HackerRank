19840646d183c6e8f497effd660cf53073856941
abisheksudarshan <abishek_1274@yahoo.com>
2020-07-05 10:42:27 +0530

    Update Pangrams.py

--- Problem Solving/Strings/Pangrams.py
+++ Problem Solving/Strings/Pangrams.py
@@ -5,12 +5,18 @@ import os
 import random
 import re
 import sys
+import string
 
 # Complete the pangrams function below.
 def pangrams(s):
-    s = set(s)
+    s = set(s.lower())
     s.discard(" ")
-    return "pangram" if len(s)==26 else "not pangram"
+    s.discard("\n")
+    s.discard(".")
+    #print(set(string.ascii_lowercase))
+    #print(s)
+    #print(s==set(string.ascii_lowercase))
+    return "pangram" if s==set(string.ascii_lowercase)  else "not pangram"
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
