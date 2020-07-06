a88f7f2fd4adad389563c63adc1e4dc5abf003fe
abisheksudarshan <abishek_1274@yahoo.com>
2020-07-06 08:17:50 +0530

    Create Text Wrap.py

---
+++ Python/Strings/Text Wrap.py
@@ -0,0 +1,9 @@
+import textwrap
+
+def wrap(string, max_width):
+    l=[]
+    for i in range(0,len(string),max_width):
+        l.append(string[i:i+max_width])
+    return '\n'.join(l)
+
+if __name__ == '__main__':
