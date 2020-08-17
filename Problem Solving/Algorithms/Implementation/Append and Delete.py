9bcb542e45aee2ba579992c9f49ee837f483dc1a
abisheksudarshan <abishek_1274@yahoo.com>
2020-08-17 08:22:51 +0530

    Create Append and Delete.py

---
+++ Problem Solving/Algorithms/Implementation/Append and Delete.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the appendAndDelete function below.
+def appendAndDelete(s, t, k):
+    c = 0
+    l = len(s)
+    while s[:l]!=t[:l]:
+        l-=1
+        c+=1
+    o = ((len(t)-l)+c)
+    if k<o:
+        return ("No")
+    elif (len(s)+len(t))<=k:
+        return ("Yes")
+    elif 2*len(t)<k:
+        return ("Yes")
+    elif k%2 == o%2:
+        return ("Yes")
+    else:
+        return ("No")
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    t = input()
+
+    k = int(input())
+
+    result = appendAndDelete(s, t, k)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
