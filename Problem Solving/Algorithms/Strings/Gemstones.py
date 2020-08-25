bd182b770f5470e42e1b694d7a4354170efcade9
abisheksudarshan <abishek_1274@yahoo.com>
2020-08-25 09:02:17 +0530

    Create Gemstones.py

---
+++ Problem Solving/Algorithms/Strings/Gemstones.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the gemstones function below.
+def gemstones(arr):
+
+    rocks = [set(x) for x in arr]
+    gems = set.intersection(*rocks)
+    return (len(gems))
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    arr = []
+
+    for _ in range(n):
+        arr_item = input()
+        arr.append(arr_item)
+
+    result = gemstones(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
